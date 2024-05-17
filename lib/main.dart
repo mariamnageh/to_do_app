d3b804e9eeb8887ae84095e912360922f5a16b68
Mariam Sadek <105167781+mariamnageh@users.noreply.github.com>
2024-05-17 17:50:58 +0300

    Bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './screens/home.dart';
+import 'package:instabug_flutter/instabug_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+  Instabug.init(
+    token: '36078c3fec34df6ebfe8c247cf6a93f6',
+    invocationEvents: [
+      InvocationEvent.shake,
+      InvocationEvent.none,
+      InvocationEvent.screenshot,
+      InvocationEvent.floatingButton
+    ],
+  );
+  Instabug.show();
+  Replies.show();
+  Instabug.setPrimaryColor(Colors.blue);
+  BugReporting.setFloatingButtonEdge(FloatingButtonEdge.right, 250);
+  BugReporting.setShakingThresholdForAndroid(800);
+  //Disable extended bug report mode
+
+  BugReporting.setExtendedBugReportMode(
+      ExtendedBugReportMode.enabledWithRequiredFields);
+
+//Enable extended bug report mode and make all additional fields required
+  ExtendedBugReportMode.enabledWithRequiredFields;
+//Enable extended bug report mode and make all additional fields optional
+  ExtendedBugReportMode.enabledWithOptionalFields;
+
+  //For all Report Types
+  BugReporting.setCommentMinimumCharacterCount(20);
+
+  //For specific Report Types
+  BugReporting.setCommentMinimumCharacterCount(
+      20, [ReportType.bug, ReportType.feedback, ReportType.question]);
 }
 
 class MyApp extends StatelessWidget {
